ca1c5dde663af3e2cc9936b7f7002fafbc147bab
ArrayList <123@123>
2023-11-26 21:19:59 +0800

    mc1.20.2-1.19.1

--- src/main/java/org/carpet_org_addition/mixin/rule/RedstoneWireBlockMixin.java
+++ src/main/java/org/carpet_org_addition/mixin/rule/RedstoneWireBlockMixin.java
@@ -3,10 +3,15 @@ package org.carpet_org_addition.mixin.rule;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.RedstoneWireBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.WorldAccess;
 import org.carpet_org_addition.CarpetOrgAdditionSettings;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 //红石线不会连接到打开的活板门上的红石线
 @Mixin(RedstoneWireBlock.class)
@@ -17,5 +22,12 @@ public class RedstoneWireBlockMixin {
             return null;
         }
         return instance.getBlock();
+    }
+
+    @Inject(method = "getStateForNeighborUpdate", at = @At("HEAD"), cancellable = true)
+    private void preventDrop(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos, CallbackInfoReturnable<BlockState> cir) {
+        if (CarpetOrgAdditionSettings.simpleUpdateSkipper && direction == Direction.DOWN) {
+            cir.setReturnValue(state);
+        }
     }
 }
