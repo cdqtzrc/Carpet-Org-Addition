e34e433aed3092bef0003202424781949d8e2902
cdqtzrc <3286637847@qq.com>
2023-12-21 18:21:27 +0800

    两条新规则

---
+++ src/main/java/org/carpet_org_addition/mixin/rule/EntityMixin.java
@@ -0,0 +1,35 @@
+package org.carpet_org_addition.mixin.rule;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.vehicle.BoatEntity;
+import org.carpet_org_addition.CarpetOrgAdditionSettings;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+    // 禁止特定生物在和平模式下被清除
+    @Inject(method = "discard", at = @At("HEAD"), cancellable = true)
+    private void discord(CallbackInfo ci) {
+        if (CarpetOrgAdditionSettings.disableMobPeacefulDespawn && (Entity) (Object) this instanceof MobEntity mob) {
+            if (mob.isPersistent() || mob.cannotDespawn())
+                ci.cancel();
+        }
+    }
+
+    // 登山船
+    @Inject(method = "getStepHeight", at = @At("HEAD"), cancellable = true)
+    private void getStepHeight(CallbackInfoReturnable<Float> cir) {
+        if (CarpetOrgAdditionSettings.climbingBoat
+                && (Entity) (Object) this instanceof BoatEntity boatEntity
+                && boatEntity.getControllingPassenger() instanceof PlayerEntity) {
+            cir.setReturnValue(1.0F);
+        }
+    }
+}
