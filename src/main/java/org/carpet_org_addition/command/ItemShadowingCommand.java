df782131a95d1b5963c9ac794735bd3983de13e8
chuanchen <chuanchen@git.com>
2024-01-23 13:29:07 +0800

    物品分身制作时发送消息

--- src/main/java/org/carpet_org_addition/command/ItemShadowingCommand.java
+++ src/main/java/org/carpet_org_addition/command/ItemShadowingCommand.java
@@ -9,8 +9,11 @@ import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Hand;
+import org.carpet_org_addition.CarpetOrgAddition;
 import org.carpet_org_addition.CarpetOrgAdditionSettings;
-import org.carpet_org_addition.util.CommandUtils;
+import org.carpet_org_addition.exception.NoNbtException;
+import org.carpet_org_addition.util.*;
+import org.carpet_org_addition.util.helpers.ImmutableInventory;
 
 public class ItemShadowingCommand {
     public ItemShadowingCommand() {
@@ -33,7 +36,25 @@ public class ItemShadowingCommand {
             // 主手不能为空
             throw CommandUtils.getException("carpet.commands.itemshadowing.main_hand_is_empty");
         } else if (off.isEmpty()) {
-            player.setStackInHand(Hand.OFF_HAND, player.getMainHandStack());
+            player.setStackInHand(Hand.OFF_HAND, main);
+            // 广播制作物品分身的消息
+            MessageUtils.broadcastTextMessage(context.getSource(),
+                    TextUtils.getTranslate("carpet.commands.itemshadowing.broadcast",
+                            player.getDisplayName(), main.toHoverableText()));
+            // 将玩家制作物品分身的消息写入日志
+            try {
+                ImmutableInventory inventory = InventoryUtils.getInventory(main);
+                CarpetOrgAddition.LOGGER.info(player.getName().getString() + "制作了一个"
+                        + main.getItem().getName().getString() + "的物品分身，包含" + inventory.itemCount()
+                        + "个物品，分别是：" + inventory + "，在"
+                        + StringUtils.getDimensionId(player.getWorld()) + "，坐标:["
+                        + StringUtils.getBlockPosString(player.getBlockPos()) + "]");
+            } catch (NoNbtException e) {
+                CarpetOrgAddition.LOGGER.info(player.getName().getString() + "制作了一个["
+                        + main.getItem().getName().getString() + "]的物品分身，在"
+                        + StringUtils.getDimensionId(player.getWorld()) + "，坐标:["
+                        + StringUtils.getBlockPosString(player.getBlockPos()) + "]");
+            }
             return 1;
         } else {
             // 副手必须为空
