af240c4abb91d9f2826a73988c405841a2095a27
cdqtzrc <3286637847@qq.com>
2024-02-28 20:02:45 +0800

    1.24.0

--- src/main/java/org/carpet_org_addition/CarpetOrgAdditionSettings.java
+++ src/main/java/org/carpet_org_addition/CarpetOrgAdditionSettings.java
@@ -393,12 +393,6 @@ public class CarpetOrgAdditionSettings {
     )
     public static boolean canActivatesObserver = false;
 
-    //禁用聊天数据包顺序检查
-    @Rule(
-            categories = {ORG, RuleCategory.SURVIVAL}
-    )
-    public static boolean disableOutOfOrderChatCheck = false;
-
     //禁止水结冰
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
