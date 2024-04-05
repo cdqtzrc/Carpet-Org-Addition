b8b52c7851c8c52e6de744ffe5fd14ff773bf6a1
cdqtzrc <3286637847@qq.com>
2024-04-06 00:25:22 +0800

    整理代码，修复/finder查找物品标签反馈显示错误

--- src/main/java/org/carpet_org_addition/mixin/rule/AbstractPiglinEntityMixin.java
+++ src/main/java/org/carpet_org_addition/mixin/rule/AbstractPiglinEntityMixin.java
@@ -7,9 +7,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-//禁止猪灵僵尸化
 @Mixin(AbstractPiglinEntity.class)
 public class AbstractPiglinEntityMixin {
+
+    //禁止猪灵僵尸化
     @Inject(method = "shouldZombify", at = @At("HEAD"), cancellable = true)
     private void shouldZombify(CallbackInfoReturnable<Boolean> cir) {
         if (CarpetOrgAdditionSettings.disablePiglinZombify) {
--- src/main/java/org/carpet_org_addition/mixin/rule/RedstoneWireBlockMixin.java
+++ src/main/java/org/carpet_org_addition/mixin/rule/RedstoneWireBlockMixin.java
@@ -9,7 +9,7 @@ import org.carpet_org_addition.CarpetOrgAdditionSettings;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-//红石线不会连接到打开的活板门上的红石线
+// 红石线不会连接到打开的活板门上的红石线
 @Mixin(RedstoneWireBlock.class)
 public class RedstoneWireBlockMixin {
     @WrapOperation(method = "getRenderConnectionType(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;Z)Lnet/minecraft/block/enums/WireConnection;", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getBlock()Lnet/minecraft/block/Block;"))
