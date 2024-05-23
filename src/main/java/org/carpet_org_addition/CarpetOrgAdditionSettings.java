9898611aac51ef1b4b27735ce218fbe4b0be1449
cdqtzrc <3286637847@qq.com>
2024-05-23 19:10:50 +0800

    /creeper在日志输出谁炸了谁，/navigate导航到假玩家不再广播消息

--- src/main/java/org/carpet_org_addition/CarpetOrgAdditionSettings.java
+++ src/main/java/org/carpet_org_addition/CarpetOrgAdditionSettings.java
@@ -13,359 +13,359 @@ public class CarpetOrgAdditionSettings {
 
     public static final String ORG = "Org";
 
-    //制作物品分身
+    // 制作物品分身
     @Rule(
             categories = {ORG, RuleCategory.COMMAND},
             options = {"true", "false", "ops", "0", "1", "2", "3", "4"}
     )
     public static String commandItemShadowing = "ops";
 
-    //设置基岩硬度
+    // 设置基岩硬度
     @Rule(
             categories = {ORG},
             validators = {BedrockHardnessValidator.class}
     )
     public static float setBedrockHardness = -1F;
 
-    //绑定诅咒无效化
+    // 绑定诅咒无效化
     @Rule(
             categories = {ORG, RuleCategory.SURVIVAL}
     )
     public static boolean bindingCurseInvalidation = false;
 
-    //禁用钓鱼开阔水域检测
+    // 禁用钓鱼开阔水域检测
     @Rule(
             categories = {ORG, RuleCategory.SURVIVAL}
     )
     public static boolean disableOpenOrWaterDetection = false;
 
-    //幽匿尖啸体放置时状态
+    // 幽匿尖啸体放置时状态
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean sculkShriekerCanSummon = false;
 
-    //创造玩家免疫/kill
+    // 创造玩家免疫/kill
     @Rule(
             categories = {ORG, RuleCategory.CREATIVE}
     )
     public static boolean creativeImmuneKill = false;
 
-    //掉落物不消失
+    // 掉落物不消失
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean itemNeverDespawn = false;
 
-    //滑翔时不能对方块使用烟花
+    // 滑翔时不能对方块使用烟花
     @Rule(
             categories = {ORG, RuleCategory.SURVIVAL}
     )
     public static boolean flyingUseOnBlockFirework = false;
 
-    //盯着末影人眼睛看时不会激怒末影人
+    // 盯着末影人眼睛看时不会激怒末影人
     @Rule(
             categories = {ORG, RuleCategory.SURVIVAL, RuleCategory.FEATURE}
     )
     public static boolean staringEndermanNotAngry = false;
 
-    //耕地防踩踏
+    // 耕地防踩踏
     @Rule(
             categories = {ORG, RuleCategory.SURVIVAL, RuleCategory.FEATURE}
     )
     public static boolean farmlandPreventStepping = false;
 
-    //凋零骷髅可以生成在传送门方块
+    // 凋零骷髅可以生成在传送门方块
     @Rule(
             categories = {ORG, RuleCategory.SURVIVAL}
     )
     public static boolean witherSkeletonCanSpawnToPortal = false;
 
-    //最大方块交互距离
+    // 最大方块交互距离
     @Rule(
             categories = {ORG, RuleCategory.SURVIVAL, RuleCategory.FEATURE},
             validators = {MaxBlockPlaceDistanceValidator.class}
     )
     public static double maxBlockPlaceDistance = -1;
 
-    //简易更新跳略器
+    // 简易更新跳略器
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean simpleUpdateSkipper = false;
 
-    //强化引雷
+    // 强化引雷
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean channelingIgnoreWeather = false;
 
-    //无伤末影珍珠
+    // 无伤末影珍珠
     @Rule(
             categories = {ORG, RuleCategory.SURVIVAL}
     )
     public static boolean notDamageEnderPearl = false;
 
-    //保护类魔咒兼容
+    // 保护类魔咒兼容
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean protectionEnchantmentCompatible = false;
 
-    //禁用伤害免疫
+    // 禁用伤害免疫
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean disableDamageImmunity = false;
 
-    //禁止传送门更新
+    // 禁止传送门更新
     @Rule(
             categories = {ORG, RuleCategory.CREATIVE}
     )
     public static boolean disablePortalUpdate = false;
 
-    //干草捆完全抵消摔落伤害
+    // 干草捆完全抵消摔落伤害
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean hayBlockCompleteOffsetFall = false;
 
-    //蓝冰上不能刷怪
+    // 蓝冰上不能刷怪
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean blueIceCanSpawn = false;
 
-    //禁止蝙蝠生成
+    // 禁止蝙蝠生成
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean disableBatCanSpawn = false;
 
-    //海龟蛋快速孵化
+    // 海龟蛋快速孵化
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean turtleEggFastHatch = false;
 
-    //禁止海带生长
+    // 禁止海带生长
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean disableKelpGrow = false;
 
-    //强制开启潜影盒
+    // 强制开启潜影盒
     @Rule(
             categories = {ORG, RuleCategory.SURVIVAL}
     )
     public static boolean openShulkerBoxForcibly = false;
 
-    //村民无限交易
+    // 村民无限交易
     @Rule(
             categories = {ORG, RuleCategory.SURVIVAL}
     )
     public static boolean villagerInfiniteTrade = false;
 
-    //烟花火箭使用冷却
+    // 烟花火箭使用冷却
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean fireworkRocketUseCooldown = false;
 
-    //强化激流
+    // 强化激流
     @Rule(
             categories = {ORG, RuleCategory.SURVIVAL}
     )
     public static boolean riptideIgnoreWeather = false;
 
-    //禁止猪灵僵尸化
+    // 禁止猪灵僵尸化
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean disablePiglinZombify = false;
 
-    //禁止村民女巫化
+    // 禁止村民女巫化
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean disableVillagerWitch = false;
 
-    //禁止铁傀儡攻击玩家
+    // 禁止铁傀儡攻击玩家
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean disableIronGolemAttackPlayer = false;
 
-    //将镐作为基岩的有效采集工具
+    // 将镐作为基岩的有效采集工具
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean pickaxeMinedBedrock = false;
 
-    //村民回血
+    // 村民回血
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean villagerHeal = false;
 
-    //假玩家回血
+    // 假玩家回血
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean fakePlayerHeal = false;
 
-    //伤害附魔兼容
+    // 伤害附魔兼容
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean damageEnchantmentCompatible = false;
 
-    //假玩家工具命令
+    // 假玩家工具命令
     @Rule(
             categories = {ORG, RuleCategory.COMMAND},
             options = {"true", "false", "ops", "0", "1", "2", "3", "4"}
     )
     public static String commandPlayerTools = "ops";
 
-    //禁止岩浆怪生成于下界荒地
+    // 禁止岩浆怪生成于下界荒地
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean disableMagmaCubeSpawnNetherWastes = false;
 
-    //最大方块交互距离适用于实体
+    // 最大方块交互距离适用于实体
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean maxBlockPlaceDistanceReferToEntity = false;
 
-    //可再生迅捷潜行
+    // 可再生迅捷潜行
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean renewableSwiftSneak = false;
 
-    //击退棒
+    // 击退棒
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean knockbackStick = false;
 
-    //禁止重生方块爆炸
+    // 禁止重生方块爆炸
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean disableRespawnBlocksExplode = false;
 
-    //CCE更新抑制器
+    // CCE更新抑制器
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean CCEUpdateSuppression = false;
 
-    //开放/seed命令权限
+    // 开放/seed命令权限
     @Rule(
             categories = {ORG, RuleCategory.COMMAND}
     )
     public static boolean openSeedPermissions = false;
 
-    //发送消息命令
+    // 发送消息命令
     @Rule(
             categories = {ORG, RuleCategory.COMMAND},
             options = {"true", "false", "ops", "0", "1", "2", "3", "4"}
     )
     public static String commandSendMessage = "ops";
 
-    //开放/carpet命令权限
+    // 开放/carpet命令权限
     @Rule(
             categories = {ORG, RuleCategory.COMMAND, RuleCategory.CLIENT}
     )
     public static boolean openCarpetPermissions = false;
 
-    //开放/gamerule命令权限
+    // 开放/gamerule命令权限
     @Rule(
             categories = {ORG, RuleCategory.COMMAND}
     )
     public static boolean openGameRulePermissions = false;
 
-    //打开村民物品栏
+    // 打开村民物品栏
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean openVillagerInventory = false;
 
-    //和平的苦力怕
+    // 和平的苦力怕
     @Rule(
             categories = {ORG, RuleCategory.SURVIVAL}
     )
     public static boolean peacefulCreeper = false;
 
-    //经验转移
+    // 经验转移
     @Rule(
             categories = {ORG, RuleCategory.COMMAND},
             options = {"true", "false", "ops", "0", "1", "2", "3", "4"}
     )
     public static String commandXpTransfer = "ops";
 
-    //生存旁观切换命令
+    // 生存旁观切换命令
     @Rule(
             categories = {ORG, RuleCategory.COMMAND},
             options = {"true", "false", "ops", "0", "1", "2", "3", "4"}
     )
     public static String commandSpectator = "ops";
 
-    //方块查找器命令
+    // 查找器命令
     @Rule(
             categories = {ORG, RuleCategory.COMMAND},
             options = {"true", "false", "ops", "0", "1", "2", "3", "4"}
     )
     public static String commandFinder = "ops";
 
-    //自杀
+    // 自杀
     @Rule(
             categories = {ORG, RuleCategory.COMMAND},
             options = {"true", "false", "ops", "0", "1", "2", "3", "4"}
     )
     public static String commandKillMe = "ops";
 
-    //路径点管理器
+    // 路径点管理器
     @Rule(
             categories = {ORG, RuleCategory.COMMAND},
             options = {"true", "false", "ops", "0", "1", "2", "3", "4"}
     )
     public static String commandLocations = "ops";
 
-    //生命值不满可以进食
+    // 生命值不满可以进食
     @Rule(
             categories = {ORG, RuleCategory.SURVIVAL}
     )
     public static boolean healthNotFullCanEat = false;
 
-    //可采集刷怪笼
+    // 可采集刷怪笼
     @Rule(
             categories = {ORG, RuleCategory.SURVIVAL}
     )
     public static boolean canMineSpawner = false;
 
-    //假玩家生成时无击退
+    // 假玩家生成时无击退
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
     public static boolean fakePlayerSpawnNoKnockback = false;
 
-    //可激活侦测器
+    // 可激活侦测器
     @Rule(
             categories = {ORG, RuleCategory.FEATURE, RuleCategory.SURVIVAL}
     )
     public static boolean canActivatesObserver = false;
 
-    //禁用聊天数据包顺序检查
+    // 禁用聊天数据包顺序检查
     @Rule(
             categories = {ORG, RuleCategory.SURVIVAL}
     )
     public static boolean disableOutOfOrderChatCheck = false;
 
-    //禁止水结冰
+    // 禁止水结冰
     @Rule(
             categories = {ORG, RuleCategory.FEATURE}
     )
--- src/main/java/org/carpet_org_addition/command/CreeperCommand.java
+++ src/main/java/org/carpet_org_addition/command/CreeperCommand.java
@@ -2,6 +2,8 @@ package org.carpet_org_addition.command;
 
 import carpet.utils.CommandHelper;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.CreeperEntity;
@@ -9,7 +11,9 @@ import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.World;
+import org.carpet_org_addition.CarpetOrgAddition;
 import org.carpet_org_addition.CarpetOrgAdditionSettings;
+import org.carpet_org_addition.util.CommandUtils;
 import org.carpet_org_addition.util.MathUtils;
 
 public class CreeperCommand {
@@ -17,24 +21,27 @@ public class CreeperCommand {
         dispatcher.register(CommandManager.literal("creeper")
                 .requires(source -> CommandHelper.canUseCommand(source, CarpetOrgAdditionSettings.commandCreeper))
                 .then(CommandManager.argument("player", EntityArgumentType.player())
-                        .executes(context -> creeperExplosion(EntityArgumentType.getPlayer(context, "player")))));
+                        .executes(CreeperCommand::creeperExplosion)));
     }
 
     // 创建苦力怕并爆炸
-    private static int creeperExplosion(ServerPlayerEntity player) {
-        World world = player.getWorld();
+    private static int creeperExplosion(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        ServerPlayerEntity targetPlayer = CommandUtils.getArgumentPlayer(context);
+        World world = targetPlayer.getWorld();
         // 创建苦力怕对象
         CreeperEntity creeper = new CreeperEntity(EntityType.CREEPER, world);
-        try {
-            // 产生爆炸
-            player.getWorld().createExplosion(creeper,
-                    player.getX() + MathUtils.randomInt(-3, 3),
-                    player.getY() + MathUtils.randomInt(-1, 1),
-                    player.getZ() + MathUtils.randomInt(-3, 3),
-                    3F, false, World.ExplosionSourceType.NONE);
-        } finally {
-            // 删除这只苦力怕
-            creeper.discard();
+        // 产生爆炸
+        targetPlayer.getWorld().createExplosion(creeper,
+                targetPlayer.getX() + MathUtils.randomInt(-3, 3),
+                targetPlayer.getY() + MathUtils.randomInt(-1, 1),
+                targetPlayer.getZ() + MathUtils.randomInt(-3, 3),
+                3F, false, World.ExplosionSourceType.NONE);
+        // 删除这只苦力怕
+        creeper.discard();
+        ServerPlayerEntity sourcePlayer = context.getSource().getPlayer();
+        if (sourcePlayer != null) {
+            CarpetOrgAddition.LOGGER.info(sourcePlayer.getName().getString() + "在" + targetPlayer.getName().getString()
+                    + "周围制造了一场苦力怕爆炸");
         }
         return 1;
     }
