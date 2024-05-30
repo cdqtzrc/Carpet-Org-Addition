75c15e73458f1f492e769681734a428905a54fb6
cdqtzrc <3286637847@qq.com>
2024-05-30 22:53:02 +0800

    添加未使用的物品栏整理代码

--- src/main/java/org/carpet_org_addition/util/fakeplayer/PlayerEnderChestScreenHandler.java
+++ src/main/java/org/carpet_org_addition/util/fakeplayer/PlayerEnderChestScreenHandler.java
@@ -2,21 +2,19 @@ package org.carpet_org_addition.util.fakeplayer;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.inventory.Inventory;
 import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 // 假玩家末影箱GUI
-public class FakePlayerEnderChestScreenHandler extends GenericContainerScreenHandler {
+public class PlayerEnderChestScreenHandler extends GenericContainerScreenHandler {
     /**
      * 不一定是假玩家，也有可能是/playerTools命令的执行者自己
      */
     private final ServerPlayerEntity playerEntity;
 
-    public FakePlayerEnderChestScreenHandler(int syncId, PlayerInventory playerInventory,
-                                             Inventory inventory, ServerPlayerEntity playerEntity) {
-        super(ScreenHandlerType.GENERIC_9X3, syncId, playerInventory, inventory, 3);
+    public PlayerEnderChestScreenHandler(int syncId, PlayerInventory playerInventory, ServerPlayerEntity playerEntity) {
+        super(ScreenHandlerType.GENERIC_9X3, syncId, playerInventory, playerEntity.getEnderChestInventory(), 3);
         this.playerEntity = playerEntity;
     }
 
---
+++ src/main/java/org/carpet_org_addition/util/helpers/ShulkerBoxInventory.java
@@ -0,0 +1,131 @@
+package org.carpet_org_addition.util.helpers;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.util.collection.DefaultedList;
+import org.carpet_org_addition.util.InventoryUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ShulkerBoxInventory implements Inventory {
+
+    private final DefaultedList<ItemStack> stacks;
+    private final List<ItemStack> shulkerBoxList;
+
+    public ShulkerBoxInventory(List<ItemStack> shulkerBoxList) {
+        this.shulkerBoxList = new ArrayList<>();
+        ArrayList<ItemStack> list = new ArrayList<>();
+        for (ItemStack itemStack : shulkerBoxList) {
+            if (InventoryUtils.isShulkerBoxItem(itemStack)) {
+                if (InventoryUtils.isEmptyShulkerBox(itemStack)) {
+                    continue;
+                }
+                this.shulkerBoxList.add(itemStack);
+                // 获取潜影盒NBT
+                NbtCompound nbt = Objects.requireNonNull(itemStack.getNbt()).getCompound(InventoryUtils.BLOCK_ENTITY_TAG);
+                if (nbt != null && nbt.contains(InventoryUtils.ITEMS, NbtElement.LIST_TYPE)) {
+                    DefaultedList<ItemStack> defaultedList = DefaultedList.ofSize(27, ItemStack.EMPTY);
+                    // 读取潜影盒NBT
+                    Inventories.readNbt(nbt, defaultedList);
+                    list.addAll(defaultedList);
+                }
+            }
+        }
+        this.stacks = DefaultedList.copyOf(ItemStack.EMPTY, list.toArray(value -> new ItemStack[0]));
+    }
+
+    @Override
+    public int size() {
+        return stacks.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        for (ItemStack itemStack : this.stacks) {
+            if (itemStack.isEmpty()) {
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public ItemStack getStack(int slot) {
+        return this.stacks.get(slot);
+    }
+
+    @Override
+    public ItemStack removeStack(int slot, int amount) {
+        return Inventories.splitStack(this.stacks, slot, amount);
+    }
+
+    @Override
+    public ItemStack removeStack(int slot) {
+        return Inventories.removeStack(this.stacks, slot);
+    }
+
+    @Override
+    public void setStack(int slot, ItemStack stack) {
+        this.stacks.set(slot, stack);
+    }
+
+    @Override
+    public void markDirty() {
+    }
+
+    @Override
+    public boolean canPlayerUse(PlayerEntity player) {
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        this.stacks.clear();
+    }
+
+    /**
+     * 为潜影盒内的物品整理并排序，当前物品栏为多个潜影盒时，物品可以跨潜影盒整理排序
+     */
+    public void sort() {
+        InventoryUtils.sortInventory(this.stacks);
+    }
+
+    /**
+     * 清除空潜影盒的物品栏NBT
+     */
+    public void removeInventoryNbt() {
+        for (ItemStack itemStack : this.shulkerBoxList) {
+            if (InventoryUtils.isEmptyShulkerBox(itemStack)) {
+                itemStack.removeSubNbt(InventoryUtils.BLOCK_ENTITY_TAG);
+            }
+        }
+    }
+
+    /**
+     * 应用对潜影盒的更改：将物品集合写入NBT
+     */
+    public void application() {
+        int number = 0;
+        for (ItemStack itemStack : this.shulkerBoxList) {
+            NbtCompound nbt = new NbtCompound();
+            DefaultedList<ItemStack> defaultedList;
+            if (number < this.size()) {
+                List<ItemStack> list = this.stacks.subList(number, (number + 27) > this.size() ? this.size() : (number + 27));
+                number += 27;
+                ItemStack[] arr = list.toArray(new ItemStack[0]);
+                defaultedList = DefaultedList.copyOf(ItemStack.EMPTY, arr);
+            } else {
+                defaultedList = DefaultedList.ofSize(27, ItemStack.EMPTY);
+            }
+            nbt.put(InventoryUtils.BLOCK_ENTITY_TAG, Inventories.writeNbt(new NbtCompound(), defaultedList));
+            itemStack.setNbt(nbt);
+        }
+    }
+}
