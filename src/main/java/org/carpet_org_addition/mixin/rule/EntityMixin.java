0bc131a06491aab6033107e585bfd503bf1cbfdb
cdqtzrc <3286637847@qq.com>
2024-06-19 18:25:00 +0800

    修复禁止和平模式下持久生物被清除规则启用时生物转化不会删除旧的实体

--- src/main/java/org/carpet_org_addition/mixin/rule/EntityMixin.java
+++ src/main/java/org/carpet_org_addition/mixin/rule/EntityMixin.java
@@ -1,7 +1,6 @@
 package org.carpet_org_addition.mixin.rule;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.vehicle.BoatEntity;
 import org.carpet_org_addition.CarpetOrgAdditionSettings;
@@ -9,7 +8,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
@@ -17,16 +15,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class EntityMixin {
     @Unique
     private final Entity thisEntity = (Entity) (Object) this;
-
-    // 禁止特定生物在和平模式下被清除
-    @Inject(method = "discard", at = @At("HEAD"), cancellable = true)
-    private void discord(CallbackInfo ci) {
-        if (CarpetOrgAdditionSettings.disableMobPeacefulDespawn && thisEntity instanceof MobEntity mob) {
-            if (mob.isPersistent() || mob.cannotDespawn()) {
-                ci.cancel();
-            }
-        }
-    }
 
     // 登山船
     @Inject(method = "getStepHeight", at = @At("HEAD"), cancellable = true)
