6813bc070e20bb6df428910aad089f27ce2dbf42
wendavid552 <david.jianwei.wen@gmail.com>
2024-08-03 12:08:08 +0800

    :green_heart: 引入ConditionalMixin，添加fabric-networking-api的兼容

--- src/main/java/org/carpet_org_addition/mixin/compat/fabricapi/AbstractNetworkAddonMixin.java
+++ src/main/java/org/carpet_org_addition/mixin/compat/fabricapi/AbstractNetworkAddonMixin.java
@@ -2,14 +2,18 @@ package org.carpet_org_addition.mixin.compat.fabricapi;
 
 import carpet.patches.FakeClientConnection;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import net.fabricmc.fabric.impl.networking.AbstractChanneledNetworkAddon;
 import net.fabricmc.fabric.impl.networking.AbstractNetworkAddon;
 import net.fabricmc.fabric.impl.networking.GlobalReceiverRegistry;
-import net.fabricmc.fabric.impl.networking.server.ServerPlayNetworkAddon;
+import org.carpet_org_addition.util.ModIds;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 
+@Restriction(
+        require = @Condition(ModIds.FABRIC_NETWORKING_API)
+)
 @Mixin(value = AbstractNetworkAddon.class, priority = 998, remap = false)
 public abstract class AbstractNetworkAddonMixin {
     @WrapWithCondition(
