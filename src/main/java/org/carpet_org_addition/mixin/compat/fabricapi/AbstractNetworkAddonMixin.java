ae8458105737e9a3f44b95f2608d9de29c6f5926
cdqtzrc <3286637847@qq.com>
2024-08-03 11:07:52 +0800

    代码风格

--- src/main/java/org/carpet_org_addition/mixin/compat/fabricapi/AbstractNetworkAddonMixin.java
+++ src/main/java/org/carpet_org_addition/mixin/compat/fabricapi/AbstractNetworkAddonMixin.java
@@ -7,25 +7,25 @@ import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import net.fabricmc.fabric.impl.networking.AbstractChanneledNetworkAddon;
 import net.fabricmc.fabric.impl.networking.AbstractNetworkAddon;
 import net.fabricmc.fabric.impl.networking.GlobalReceiverRegistry;
-import org.carpet_org_addition.util.ModIds;
+import net.minecraft.network.ClientConnection;
+import org.carpet_org_addition.util.constant.ModIds;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Restriction(
-        require = @Condition(ModIds.FABRIC_NETWORKING_API)
-)
+@SuppressWarnings("UnstableApiUsage")
+@Restriction(require = @Condition(ModIds.FABRIC_NETWORKING_API))
 @Mixin(value = AbstractNetworkAddon.class, priority = 998, remap = false)
 public abstract class AbstractNetworkAddonMixin {
-    @WrapWithCondition(
-            method = "lateInit",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/fabricmc/fabric/impl/networking/GlobalReceiverRegistry;startSession(Lnet/fabricmc/fabric/impl/networking/AbstractNetworkAddon;)V"
-            )
-    )
-    boolean notStartSession_ifFakeClientConnection(GlobalReceiverRegistry instance, AbstractNetworkAddon<?> addon) {
+    @SuppressWarnings("RedundantIfStatement")
+    @WrapWithCondition(method = "lateInit", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/impl/networking/GlobalReceiverRegistry;startSession(Lnet/fabricmc/fabric/impl/networking/AbstractNetworkAddon;)V"))
+    private boolean notStartSession_ifFakeClientConnection(GlobalReceiverRegistry<?> instance, AbstractNetworkAddon<?> addon) {
+        // 修复fabric api和Carpet的内存泄漏问题
+        // TODO 规则控制是否启用修复
         if (addon instanceof AbstractChanneledNetworkAddon<?>) {
-            return !(((AbstractChanneledNetworkAddonInvoker) addon).getConnection() instanceof FakeClientConnection);
+            ClientConnection connection = ((AbstractChanneledNetworkAddonInvoker) addon).getConnection();
+            if (connection instanceof FakeClientConnection) {
+                return false;
+            }
         }
         return true;
     }
