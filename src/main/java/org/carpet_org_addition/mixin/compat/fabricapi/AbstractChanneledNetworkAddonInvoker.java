d0372c7b7add31e1d091f1c9f823836843840bd5
wendavid552 <david.jianwei.wen@gmail.com>
2024-08-03 12:08:08 +0800

    :green_heart: 引入ConditionalMixin，添加fabric-networking-api的兼容

--- src/main/java/org/carpet_org_addition/mixin/compat/fabricapi/AbstractChanneledNetworkAddonInvoker.java
+++ src/main/java/org/carpet_org_addition/mixin/compat/fabricapi/AbstractChanneledNetworkAddonInvoker.java
@@ -1,10 +1,16 @@
 package org.carpet_org_addition.mixin.compat.fabricapi;
 
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import net.fabricmc.fabric.impl.networking.AbstractChanneledNetworkAddon;
 import net.minecraft.network.ClientConnection;
+import org.carpet_org_addition.util.ModIds;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+@Restriction(
+        require = @Condition(ModIds.FABRIC_NETWORKING_API)
+)
 @Mixin(value = AbstractChanneledNetworkAddon.class, remap = false)
 public interface AbstractChanneledNetworkAddonInvoker {
     @Accessor("connection")
