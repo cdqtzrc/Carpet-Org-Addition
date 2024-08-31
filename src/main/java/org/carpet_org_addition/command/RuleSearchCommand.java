a306116fbea8ab8cef49cd006977f53c6ccfb71d
cdqtzrc <3286637847@qq.com>
2024-08-31 22:53:35 +0800

    /ruleSearch支持正则表达式

--- src/main/java/org/carpet_org_addition/command/RuleSearchCommand.java
+++ src/main/java/org/carpet_org_addition/command/RuleSearchCommand.java
@@ -6,6 +6,7 @@ import carpet.api.settings.RuleHelper;
 import carpet.utils.CommandHelper;
 import carpet.utils.Messenger;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.server.command.CommandManager;
@@ -16,18 +17,20 @@ import org.carpet_org_addition.mixin.rule.carpet.SettingsManagerAccessor;
 import org.carpet_org_addition.util.TextUtils;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class RuleSearchCommand {
-    // TODO 支持正则表达式
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(CommandManager.literal("ruleSearch")
                 .requires(source -> CommandHelper.canUseCommand(source, CarpetOrgAdditionSettings.commandRuleSearch))
                 .then(CommandManager.argument("rule", StringArgumentType.string())
-                        .executes(RuleSearchCommand::listRule)));
+                        .executes(context -> listRule(context, false))
+                        .then(CommandManager.argument("regex", BoolArgumentType.bool())
+                                .executes(context -> listRule(context, BoolArgumentType.getBool(context, "regex"))))));
     }
 
     // 列出符合条件的规则
-    private static int listRule(CommandContext<ServerCommandSource> context) {
+    private static int listRule(CommandContext<ServerCommandSource> context, boolean regex) {
         String rule = StringArgumentType.getString(context, "rule");
         if (CarpetServer.settingsManager == null) {
             return 0;
@@ -38,13 +41,22 @@ public class RuleSearchCommand {
         text.styled(style -> style.withBold(true));
         context.getSource().sendFeedback(() -> text, false);
         int ruleCount = 0;
-        for (CarpetRule<?> carpet : list) {
-            if (RuleHelper.translatedName(carpet).contains(rule)) {
+        for (CarpetRule<?> carpetRule : list) {
+            if (matcher(rule, RuleHelper.translatedName(carpetRule), regex)) {
                 Messenger.m(context.getSource(),
-                        ((SettingsManagerAccessor) CarpetServer.settingsManager).displayInteractiveSettings(carpet));
+                        ((SettingsManagerAccessor) CarpetServer.settingsManager).displayInteractiveSettings(carpetRule));
                 ruleCount++;
             }
         }
         return ruleCount;
+    }
+
+    // Carpet规则名是否包含指定子字符串
+    private static boolean matcher(String rule, String translatedName, boolean regex) {
+        if (regex) {
+            // 检查规则名称是否包含与正则表达式匹配的子字符串
+            return Pattern.compile(rule).matcher(translatedName).find();
+        }
+        return translatedName.contains(rule);
     }
 }
