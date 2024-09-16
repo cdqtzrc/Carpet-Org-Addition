4675a572587959ee7de81d0df42ef873a0caaa08
cdqtzrc <3286637847@qq.com>
2024-09-17 01:24:00 +0800

    客户端提示框

---
+++ src/main/java/org/carpet_org_addition/mixin/util/client/InGameHudMixin.java
@@ -0,0 +1,15 @@
+package org.carpet_org_addition.mixin.util.client;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+    @Inject(method = "render", at = @At("HEAD"))
+    private void render(DrawContext context, float tickDelta, CallbackInfo ci) {
+    }
+}
