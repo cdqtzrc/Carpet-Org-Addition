a406f31f38c768658c024e8b717fc0acd779220d
cdqtzrc <3286637847@qq.com>
2024-09-29 22:54:11 +0800

    CCE更新抑制器日志输出

--- src/main/java/org/carpet_org_addition/mixin/rule/ShulkerBoxBlockMixin.java
+++ src/main/java/org/carpet_org_addition/mixin/rule/ShulkerBoxBlockMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.carpet_org_addition.CarpetOrgAdditionSettings;
+import org.carpet_org_addition.exception.CCEUpdateSuppressException;
 import org.carpet_org_addition.util.MessageUtils;
 import org.carpet_org_addition.util.TextUtils;
 import org.carpet_org_addition.util.WorldUtils;
@@ -37,8 +38,7 @@ public class ShulkerBoxBlockMixin {
     private void getComparatorOutput(BlockState state, World world, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
         //!world.isClient： 更新抑制不在客户端进行，防止客户端游戏崩溃
         if (canUpdateSuppression(world, pos) && !world.isClient) {
-            throw new ClassCastException("类型转换异常，在:"
-                    + world.getRegistryKey().getValue() + " " + WorldUtils.toPosString(pos));
+            throw new CCEUpdateSuppressException(pos, "CCE triggered on " + WorldUtils.toWorldPosString(world, pos));
         }
     }
 
