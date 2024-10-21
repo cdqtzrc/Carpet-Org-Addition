b796a32aca269ccb389825ffd413a1e91bbc12fe
cdqtzrc <3286637847@qq.com>
2024-10-21 15:22:06 +0800

    过时的注释和注解

--- src/main/java/org/carpetorgaddition/rule/value/BetterTotemOfUndying.java
+++ src/main/java/org/carpetorgaddition/rule/value/BetterTotemOfUndying.java
@@ -1,7 +1,5 @@
 package org.carpetorgaddition.rule.value;
 
-@SuppressWarnings("unused")
 public enum BetterTotemOfUndying {
-    // 枚举常量SHULKER_BOX在代码中没有被使用，也不需要被使用
     TRUE, FALSE, SHULKER_BOX
 }
